8ca40eb9456bb08ca6bfc6127057d5d506c3ee5e
Yato <kaung@rangoon.tech>
2023-11-14 21:01:59 +0630

    feat(position): change position of the wheel

--- src/Components/Vehicle/Vehicle.jsx
+++ src/Components/Vehicle/Vehicle.jsx
@@ -1,11 +1,17 @@
 import Body from "./Body"
+import Wheel from "./Wheel"
 
 const Vehicle = () => {
+
+    
 
     return(
         <>
             <Body/>
-            {/* <VehicleWheel/> */}
+            <Wheel position={[-0.6,0.3,0.75]}/>
+            <Wheel position={[0.6,0.3,0.75]} left={true}/>
+            <Wheel position={[-0.6,0.3,-0.55]}/>
+            <Wheel position={[0.6,0.3,-0.55]} left={true}/>
         </>
     )
 }
--- src/Components/Vehicle/Wheel.jsx
+++ src/Components/Vehicle/Wheel.jsx
@@ -1,10 +1,9 @@
 import { useGLTF, Center } from "@react-three/drei"
 
-const Wheel = () => {
+const Wheel = ({position, left}) => {
     const {nodes, materials} = useGLTF("./assets/suv.gltf")
-
     return(
-        <group>
+        <group position={position} rotation-y={left? Math.PI : 0}>
             <Center>
                 <mesh 
                     geometry={nodes.Mesh_wheel_frontLeft004.geometry} 
