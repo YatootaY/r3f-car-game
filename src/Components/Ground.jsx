acb02549810778c810923fee0b29574772a32ca3
Yato <kaung@rangoon.tech>
2023-11-16 22:48:34 +0630

    feat(font): added font to text

--- src/Components/Ground.jsx
+++ src/Components/Ground.jsx
@@ -1,5 +1,6 @@
 import { usePlane } from "@react-three/cannon"
 import Walls from "./Walls"
+import { Text } from "@react-three/drei"
 
 const Ground = () => {
 
@@ -8,6 +9,8 @@ const Ground = () => {
         rotation: [-Math.PI / 2, 0,0]}
     ))
 
+    const color = "#6a2e00"
+
     return(
         <>
             <mesh receiveShadow ref={ref}>
@@ -15,6 +18,22 @@ const Ground = () => {
                 <meshStandardMaterial color="#f79000"/>
             </mesh>
             <Walls/>
+            <mesh
+                position={[8,0.01,10]} 
+                rotation={[0, Math.PI / 2, 0]}
+            >
+                <Text
+                    font="./assets/kanit-v15-latin-600.woff"
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    anchorX="center"
+                    anchorY="center"
+                    fontSize={1}
+                    lineHeight={1.2}
+                >
+                    {`W - Forward\nS - Backward\nA - Turn Left\nD - Turn Right\nSpace - Brake`}
+                    <meshBasicMaterial color={color}/>
+                </Text>
+            </mesh>
         </>
     )
 }
--- src/Components/Vehicle/Vehicle.jsx
+++ src/Components/Vehicle/Vehicle.jsx
@@ -12,7 +12,7 @@ const Vehicle = () => {
     const width = 1.5
     const height = 1
     const front = 1.4
-    const position = [0,2,10]
+    const position = [12,2,15]
     const wheelRadius = 0.3;
 
     const chassisBodyArgs = [width, height, front * 2]
@@ -22,7 +22,7 @@ const Vehicle = () => {
         args: chassisBodyArgs,
         mass: 150,
         position,
-        rotation: [0, Math.PI, 0]
+        rotation: [0, -Math.PI/2, 0]
     }), useRef())
 
     const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius)
