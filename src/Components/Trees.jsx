c5bee7bb77fec5f4c2b51322f342895eb86e5387
Yato <kaung@rangoon.tech>
2023-11-17 13:51:32 +0630

    feat(objects): added camp

--- src/Components/Trees.jsx
+++ src/Components/Trees.jsx
@@ -1,10 +1,54 @@
+import { useBox } from "@react-three/cannon"
+import { useGLTF, Clone } from "@react-three/drei"
 
 
 const Trees = () => {
 
+    const tree1 = useGLTF("./assets/tree_blocks.glb")
+    const tree2 = useGLTF("./assets/tree_detailed.glb")
+    const tree3 = useGLTF("./assets/tree_plateau.glb")
+
+    const [tree1Body1] = useBox(() => ({
+        mass: 1,
+        args: [2,4.6,2],
+        position: [16,2.3,7]
+    }))
+
+    const [tree1Body2] = useBox(() => ({
+        mass: 1,
+        args: [2,4.6,2],
+        position: [-4,2.3,4]
+    }))
+
+    const [tree2Body1] = useBox(() => ({
+        mass: 1,
+        args: [2,5.2,2],
+        position: [3,2.6,1]
+    }))
+
+    const [tree3Body1] = useBox(() => ({
+        mass: 1,
+        args: [1.8,5.2,1.8],
+        position: [-10,2.6,-5]
+    }))
+
     return(
         <>
-        
+            <mesh ref={tree1Body1}>
+                <Clone object={tree1.scene} scale={4} position-y={-2.2}/>
+            </mesh>
+
+            <mesh ref={tree1Body2}>
+                <Clone object={tree1.scene} scale={4} position-y={-2.2}/>
+            </mesh>
+
+            <mesh ref={tree2Body1}>
+                <Clone object={tree2.scene} scale={4} position-y={-2.4}/>
+            </mesh>
+
+            <mesh ref={tree3Body1}>
+                <Clone object={tree3.scene} scale={4} position-y={-2.4}/>
+            </mesh>
         </>
     )
 }
