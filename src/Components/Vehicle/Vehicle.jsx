9147c9f52681ff1076a705688ebd953977065ffd
Yato <kaung@rangoon.tech>
2023-11-21 13:42:49 +0630

    feat(audio): car engine sound positional audio

--- src/Components/Vehicle/Vehicle.jsx
+++ src/Components/Vehicle/Vehicle.jsx
@@ -1,19 +1,58 @@
 /* eslint-disable no-unused-vars */
 import { useBox, useRaycastVehicle } from "@react-three/cannon"
 import { useRef } from "react"
-import { useGLTF, useKeyboardControls, Center } from "@react-three/drei"
+import { useGLTF, useKeyboardControls, Center, PositionalAudio } from "@react-three/drei"
 import useWheels from "./useWheels"
 import { useFrame } from "@react-three/fiber"
+import { useEffect } from "react"
+import { useState } from "react"
 
 const Vehicle = () => {
 
     const car = useGLTF("./assets/car.gltf")
+    const carEngineSound = useRef()
+    const [play, setPlay] = useState(false)
 
     const width = 1.5
     const height = 1
     const front = 1.4
     const position = [12,2,15]
     const wheelRadius = 0.3;
+
+    useEffect(() => {
+        const audio = carEngineSound.current;
+        let fadeInInterval;
+        let fadeOutInterval
+    
+        if (play) {
+            audio.play();
+            audio.setVolume(0);
+    
+            fadeInInterval = setInterval(() => {
+                const currentVolume = audio.getVolume();
+                if (currentVolume < 0.3) {
+                    audio.setVolume(currentVolume + 0.01);
+                } else {
+                    clearInterval(fadeInInterval);
+                }
+            }, 200);
+        } else {
+            fadeOutInterval = setInterval(() => {
+                const currentVolume = audio.getVolume();
+                if (currentVolume > 0.1) {
+                    audio.setVolume(currentVolume - 0.01);
+                } else {
+                    audio.stop();
+                    clearInterval(fadeOutInterval);
+                }
+            }, 100);
+        }
+    
+        return () => {
+            clearInterval(fadeInInterval);
+            clearInterval(fadeOutInterval);
+        };
+    }, [play]);
 
     const chassisBodyArgs = [width, height, front * 2]
 
@@ -35,6 +74,17 @@ const Vehicle = () => {
 
     const [subscribeKey, getKeys] = useKeyboardControls()
 
+    useEffect(() => {
+        console.log(chassisApi)
+        const unsubscribe = chassisApi.position.subscribe((p) => {
+            carEngineSound.current.position.copy({x: p[0], y: p[1], z:p[2]})
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    },[])
+
     useFrame((state, delta) => {
         const {forward, backward, leftward, rightward, brake, boost } = getKeys()
 
@@ -49,12 +99,15 @@ const Vehicle = () => {
         if (forward) {
             vehicleApi.applyEngineForce(-enginePower, 1);
             vehicleApi.applyEngineForce(-enginePower, 0);
+            setPlay(true)
         } else if (backward) {
             vehicleApi.applyEngineForce(enginePower, 1);
             vehicleApi.applyEngineForce(enginePower, 0);
+            setPlay(true)
         } else {
             vehicleApi.applyEngineForce(0, 1);
             vehicleApi.applyEngineForce(0, 0);
+            setPlay(false)
         }
 
         if (leftward) {
@@ -77,6 +130,13 @@ const Vehicle = () => {
 
     return(
         <>
+            <PositionalAudio
+                url="./assets/engineSound.wav"
+                loop
+                autoplay={false}
+                distance={25}
+                ref={carEngineSound}
+            />
             <group ref={vehicle}>
                 <group ref={chassisBody}>
                     <primitive object={car.nodes.body004} position={[0,-0.6,0]} />
