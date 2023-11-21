11f4c2f3eb170f9267aa68e7545a6e8e28a4fc1f
Yato <kaung@rangoon.tech>
2023-11-21 22:16:17 +0630

    feat(keys): added key geo

--- src/Components/Ground.jsx
+++ src/Components/Ground.jsx
@@ -1,9 +1,9 @@
 import { usePlane } from "@react-three/cannon"
 import Walls from "./Walls"
-import { Text } from "@react-three/drei"
 import Trees from "./Trees"
 import Fence from "./Fence"
 import Camp from "./Camp"
+import Keys from "./Keys"
 
 const Ground = () => {
 
@@ -22,22 +22,7 @@ const Ground = () => {
             <Fence/>
             <Camp/>
             <Walls/>
-            <mesh
-                position={[8,0.01,10]} 
-                rotation={[0, Math.PI / 2, 0]}
-            >
-                <Text
-                    font="./assets/kanit-v15-latin-600.woff"
-                    rotation={[-Math.PI / 2, 0, 0]}
-                    anchorX="center"
-                    anchorY="center"
-                    fontSize={1}
-                    lineHeight={1.2}
-                >
-                    {`W - Forward\nS - Backward\nA - Turn Left\nD - Turn Right\nSpace - Brake`}
-                    <meshBasicMaterial color="#403e3c"/>
-                </Text>
-            </mesh>
+            <Keys/>
         </>
     )
 }
---
+++ src/Components/Keys.jsx
@@ -0,0 +1,148 @@
+import { useBox } from "@react-three/cannon"
+import {Text} from "@react-three/drei"
+import { useState } from "react"
+import * as THREE from "three"
+import { playAudio } from "../Util"
+
+const material = new THREE.MeshStandardMaterial({color:"white"})
+const Keys = () => {
+
+    const [audio] = useState(() => new Audio("./assets/hit.mp3"))
+
+    const [keyW] = useBox(() => ({
+        mass: 1,
+        args: [1.4,0.8,1.4],
+        position: [11,0.4,10],
+        onCollide: (e) => playAudio(e, audio)
+    }))
+
+    const [keyS] = useBox(() => ({
+        mass: 1,
+        args: [1.4,0.8,1.4],
+        position: [12.6,0.4,10],
+        onCollide: (e) => playAudio(e, audio)
+    }))
+
+    const [keyD] = useBox(() => ({
+        mass: 1,
+        args: [1.4,0.8,1.4],
+        position: [12.6,0.4,8.4],
+        onCollide: (e) => playAudio(e, audio)
+    }))
+
+    const [keyA] = useBox(() => ({
+        mass: 1,
+        args: [1.4,0.8,1.4],
+        position: [12.6,0.4,11.6],
+        onCollide: (e) => playAudio(e, audio)
+    }))
+
+    const [keySpace] = useBox(() => ({
+        mass: 1,
+        args: [1.4,0.8,4.6],
+        position: [14.2,0.4,10],
+        onCollide: (e) => playAudio(e, audio)
+    }))
+
+    return(
+        <>
+        
+            <mesh
+                position={[8,0.01,10]} 
+                rotation={[0, Math.PI / 2, 0]}
+            >
+                <Text
+                    font="./assets/kanit-v15-latin-600.woff"
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    anchorX="center"
+                    anchorY="center"
+                    fontSize={0.8}
+                    lineHeight={1.2}
+                >
+                    {`Control Guide`}
+                    <meshBasicMaterial color="#403e3c"/>
+                </Text>
+            </mesh>
+            <group>
+                <mesh
+                    material={material}
+                    ref={keyW}
+                >
+                    <Text
+                        font="./assets/kanit-v15-latin-600.woff"
+                        rotation={[-Math.PI / 2, 0, Math.PI/2]}
+                        position-y={0.41}
+                        fontSize={0.8}
+                    >
+                        W
+                        <meshBasicMaterial color="#403e3c"/>
+                    </Text>
+                    <boxGeometry args={[1.4,0.8,1.4]}/>
+                </mesh>
+                <mesh
+                    material={material}
+                    ref={keyS}
+                >
+                    <Text
+                        font="./assets/kanit-v15-latin-600.woff"
+                        rotation={[-Math.PI / 2, 0, Math.PI/2]}
+                        position-y={0.41}
+                        fontSize={0.8}
+                    >
+                        S
+                        <meshBasicMaterial color="#403e3c"/>
+                    </Text>
+                    <boxGeometry args={[1.4,0.8,1.4]}/>
+                </mesh>
+                <mesh
+                    material={material}
+                    ref={keyD}
+                >
+                    <Text
+                        font="./assets/kanit-v15-latin-600.woff"
+                        rotation={[-Math.PI / 2, 0, Math.PI/2]}
+                        position-y={0.41}
+                        fontSize={0.8}
+                    >
+                        D
+                        <meshBasicMaterial color="#403e3c"/>
+                    </Text>
+                    <boxGeometry args={[1.4,0.8,1.4]}/>
+                </mesh>
+                <mesh
+                    material={material}
+                    ref={keyA}
+                >
+                    <Text
+                        font="./assets/kanit-v15-latin-600.woff"
+                        rotation={[-Math.PI / 2, 0, Math.PI/2]}
+                        position-y={0.41}
+                        fontSize={0.8}
+                    >
+                        A
+                        <meshBasicMaterial color="#403e3c"/>
+                    </Text>
+                    <boxGeometry args={[1.4,0.8,1.4]}/>
+                </mesh>
+                <mesh
+                    material={material}
+                    ref={keySpace}
+                >
+                    <Text
+                        font="./assets/kanit-v15-latin-600.woff"
+                        rotation={[-Math.PI / 2, 0, Math.PI/2]}
+                        position-y={0.41}
+                        fontSize={0.7}
+                    >
+                        brake
+                        <meshBasicMaterial color="#403e3c"/>
+                    </Text>
+                    <boxGeometry args={[1.4,0.8,4.6]}/>
+                </mesh>
+                
+            </group>
+        </>
+    )
+}
+
+export default Keys
